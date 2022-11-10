78b9931a21f395192cece4ad5aa59dcb576bd54c
rubenandiapimetel <rubenandiapimentel@gmail.com>
2022-11-10 10:35:47 +0100

    correction_someshts

--- src/js/forms.js
+++ src/js/forms.js
@@ -1,6 +1,7 @@
 var veyfiname = () =>{
 	const name = document.getElementById('formname');
 	console.log(name);
+	console.log(name);
 	if(name < 2 || name > 100)
 	alert("nombre de uuario invalido");
 }
