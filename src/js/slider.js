e88b12baaae441723a833b395b57aad107e5a6ba
rubenandiapimetel <rubenandiapimentel@gmail.com>
2022-11-17 19:13:11 +0100

    fixedsomeshits

---
+++ src/js/slider.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', ()=>{
+	let slides = document.querySelectorAll('#slider #slider__fade img');
+	const size = slides.length;
+	let inicio = 0;
+
+	showimage(0);
+})
+
+const showimage = (number) =>{
+	
+}
+
+function back(){
+
+}
